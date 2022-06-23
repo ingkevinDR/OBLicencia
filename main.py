2a848050c10fd814079ee0314dfa0f7f889bf237
ingkevinDR <ing.kevindr@gmail.com>
2022-06-22 20:58:12 -0500

    v1

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+os.system("cls")
+os.system("color a")
+# SCRIPT PARA VER LA LICENCIA DE WINDOWS 10 DESDE EL CMD -- BY RC 
+print(""" 
+
+                        ██▀███   ▄████▄  
+                        ▓██ ▒ ██▒▒██▀ ▀█  
+                        ▓██ ░▄█ ▒▒▓█    ▄ 
+                        ▒██▀▀█▄  ▒▓▓▄ ▄██▒
+                        ░██▓ ▒██▒▒ ▓███▀ ░
+                        ░ ▒▓ ░▒▓░░ ░▒ ▒  ░
+                        ░▒ ░ ▒░  ░  ▒   
+                        ░░   ░ ░        
+                        ░     ░ ░      
+                                ░        
+""")
+print("""|SCRIPT BY RODI COMUNNITY - https://rodicomunnity.blogspot.com/""")
+print("")
+print("---------------------------------------------------------------")
+print("|SU LICENCIA ES: ")
+os.system("wmic path softwarelicensingservice get OA3xOriginalProductKey")
+print("---------------------------------------------------------------")
